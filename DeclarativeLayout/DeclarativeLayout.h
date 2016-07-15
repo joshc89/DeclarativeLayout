031f2b1fe0aa74d944d4f11193433de29922ceaf
Josh Campion <joshcampion89@gmail.com>
2016-07-15 13:48:16 +0100

    Initial Commit

---
+++ DeclarativeLayout/DeclarativeLayout.h
@@ -0,0 +1,19 @@
+//
+//  DeclarativeLayout.h
+//  DeclarativeLayout
+//
+//  Created by Josh Campion on 15/07/2016.
+//  Copyright © 2016 Josh Campion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeclarativeLayout.
+FOUNDATION_EXPORT double DeclarativeLayoutVersionNumber;
+
+//! Project version string for DeclarativeLayout.
+FOUNDATION_EXPORT const unsigned char DeclarativeLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeclarativeLayout/PublicHeader.h>
+
+
